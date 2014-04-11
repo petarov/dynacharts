0b8a092fae0b27a3ffc0e22e5e51385f8f28343f
Petar Petrov <petar.petrov.georgiev@gmail.com>
2014-04-11 22:58:42 +0200

    Updated test resource

--- dc.server/src/main/java/net/vexelon/dc/server/resources/HelloWorld.java
+++ dc.server/src/main/java/net/vexelon/dc/server/resources/HelloWorld.java
@@ -2,15 +2,24 @@ package net.vexelon.dc.server.resources;
 
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
  
-@Path("helloworld")
+@Path("/users/{username}")
 public class HelloWorld {
+	
     public static final String CLICHED_MESSAGE = "Hello World PETYE!";
  
-@GET
-@Produces("text/plain")
-    public String getHello() {
-        return CLICHED_MESSAGE;
-    }
+//    @GET
+//	@Produces("text/plain")
+//    public String getHello() {
+//        return CLICHED_MESSAGE;
+//    }
+    
+    @GET
+    @Produces("text/xml")
+    public String getUser(@PathParam("username") String userName) {
+    	String result = "<user id=\"1\" name=\"" + userName + "\" />";
+    	return result;
+    }    
 }
