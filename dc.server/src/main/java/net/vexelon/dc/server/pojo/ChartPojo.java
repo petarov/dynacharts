2dbc9f2163cdc2294fe8da015447fa3789f22689
Petar Petrov <petar.petrov.georgiev@gmail.com>
2014-04-12 23:17:28 +0200

    Added charts POST method to ChartsResource

---
+++ dc.server/src/main/java/net/vexelon/dc/server/pojo/ChartPojo.java
@@ -0,0 +1,12 @@
+package net.vexelon.dc.server.pojo;
+
+import javax.xml.bind.annotation.XmlRootElement;
+
+@XmlRootElement 
+public class ChartPojo {
+	
+	public String name;
+	public String color;
+	public int size;
+
+}
--- dc.server/src/main/java/net/vexelon/dc/server/resources/ChartsResource.java
+++ dc.server/src/main/java/net/vexelon/dc/server/resources/ChartsResource.java
@@ -6,6 +6,12 @@ import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+
+import com.google.gson.Gson;
+
+import net.vexelon.dc.server.pojo.ChartPojo;
  
 /**
  * Charts resources stub. 
@@ -15,14 +21,30 @@ import javax.ws.rs.Produces;
  *
  */
 @Path("/charts")
+@Consumes(MediaType.APPLICATION_JSON)
+@Produces(MediaType.APPLICATION_JSON)
 public class ChartsResource {
 	
-//	@GET
-//    @Produces({"application/json", "application/xml"})
-//    public WebResourceList getMyResources() { 
-//		
-//	}	
+	@GET
+    public Response getResources() { 
+		Response r = Response.status(Response.Status.OK)
+				.type(MediaType.APPLICATION_JSON)
+				.entity("{id: 'http://localhost:8080/test'}")
+				.build();
+		return r;
+	}	
  
+	@POST @Path("/create")
+    public Response createChart(ChartPojo chart) {
+//		System.out.println(content);
+		
+		Response r = Response.status(Response.Status.OK)
+				.type(MediaType.APPLICATION_JSON)
+//				.entity(new Gson().toJson(chart))
+				.build();
+		return r;
+	}
+	
 //    @GET
 //	@Produces("text/plain")
 //    public String getHello() {
@@ -48,10 +70,10 @@ public class ChartsResource {
 //	    return r;
 //	}
 	
-    @GET
-    @Produces("text/xml")
-    public String getChart(@PathParam("id") String chartId) {
-    	String result = "<user id=\"1\" name=\"" + chartId + "\" />";
-    	return result;
-    }    
+//    @GET
+//    @Produces("text/xml")
+//    public String getChart(@PathParam("id") String chartId) {
+//    	String result = "<user id=\"1\" name=\"" + chartId + "\" />";
+//    	return result;
+//    }    
 }
