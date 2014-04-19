d05127e59b9f2927c49beceaae284e74d9ff15a2
Petar Petrov <petar.petrov.georgiev@gmail.com>
2014-04-19 12:27:28 +0200

    Replaced xmldom with jsdom
    SVG is now rendered directly into the DOM. An HTML serialized
    data is served back to the client.

--- dc.gensrv/routes/chart.js
+++ dc.gensrv/routes/chart.js
@@ -4,47 +4,80 @@
  */
 
 var d3 = require('d3');
-var xmldom = require('xmldom');
+var jsdom = require('jsdom');
+
+// html file skull with a container div for the d3 dataviz
+var htmlStub = '<html><head></head><body><div id="dataviz-container">'
+    + '</div><script src="js/d3.v3.min.js"></script></body></html>';
+
 
 exports.test = function(req, res) {
-    newChart(res);
+    // pass the html stub to jsDom
+    jsdom.env({ features: { QuerySelector : true }, html: htmlStub, done: function(err, window) {
+        // process the html document, like if we were at client side
+
+        var el = window.document.querySelector('#dataviz-container');
+        var body = window.document.querySelector('body');
+
+
+        newChart(el);
+        var svgsrc = window.document.innerHTML;
+
+        res.send(svgsrc);
+    }});
+
 };
 
-function newChart(res) {
-    var dataset = {
-      apples: [53245, 28479, 19697, 24037, 40245],
-    };
+function newChart(el) {
 
-    var width = 460,
-        height = 300,
+    var data = [
+        {age: '<5', population: 2704659},
+        {age: '5-13', population: 4499890},
+        {age: '14-17', population: 2159981},
+        {age: '18-24', population: 3853788},
+        {age: '25-44', population: 14106543},
+        {age: '45-64', population: 8819342},
+        {age: '≥65', population: 612463}
+    ];
+
+    var width = 960,
+        height = 500,
         radius = Math.min(width, height) / 2;
 
-    var color = d3.scale.category20();
-
-    var pie = d3.layout.pie()
-        .sort(null);
+    var color = d3.scale.ordinal()
+        .range(["#98abc5", "#8a89a6", "#7b6888", "#6b486b", "#a05d56", "#d0743c", "#ff8c00"]);
 
     var arc = d3.svg.arc()
-        .innerRadius(radius - 100)
-        .outerRadius(radius - 50);
+        .outerRadius(radius - 10)
+        .innerRadius(0);
 
-    var svg = d3.select("body").append("svg")
+    var pie = d3.layout.pie()
+        .sort(null)
+        .value(function(d) { return d.population; });
+
+    var svg = d3.select(el).append("svg")
         .attr("width", width)
         .attr("height", height)
         .append("g")
         .attr("transform", "translate(" + width / 2 + "," + height / 2 + ")");
 
-    var path = svg.selectAll("path")
-        .data(pie(dataset.apples))
-        .enter().append("path")
-        .attr("fill", function(d, i) { return color(i); })
-        .attr("d", arc);
+    data.forEach(function(d) {
+        d.population = +d.population;
+    });
 
-    // get a reference to our SVG object and add the SVG NS
-    var svgGraph = d3.select('svg')
-        .attr('xmlns', 'http://www.w3.org/2000/svg');
-    var svgXML = (new xmldom.XMLSerializer()).serializeToString(svgGraph[0][0]);
-    // fs.writeFile('graph.svg', svgXML);
+    var g = svg.selectAll(".arc")
+        .data(pie(data))
+        .enter().append("g")
+        .attr("class", "arc");
 
-    res.send(svgXML);
+    g.append("path")
+        .attr("d", arc)
+        .style("fill", function(d) { return color(d.data.age); });
+
+    g.append("text")
+        .attr("transform", function(d) { return "translate(" + arc.centroid(d) + ")"; })
+        .attr("dy", ".35em")
+        .style("text-anchor", "middle")
+        .text(function(d) { return d.data.age; });
+
 }
