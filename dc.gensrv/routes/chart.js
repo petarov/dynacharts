23a184ed2ee7e42456db3f9cd078a626e288182f
Petar Petrov <petar.petrov.georgiev@gmail.com>
2014-04-19 11:47:49 +0200

    Gen server now serves a sample test chart

--- dc.gensrv/routes/chart.js
+++ dc.gensrv/routes/chart.js
@@ -4,7 +4,47 @@
  */
 
 var d3 = require('d3');
+var xmldom = require('xmldom');
 
 exports.test = function(req, res) {
-    res.send("respond with a resource");
+    newChart(res);
 };
+
+function newChart(res) {
+    var dataset = {
+      apples: [53245, 28479, 19697, 24037, 40245],
+    };
+
+    var width = 460,
+        height = 300,
+        radius = Math.min(width, height) / 2;
+
+    var color = d3.scale.category20();
+
+    var pie = d3.layout.pie()
+        .sort(null);
+
+    var arc = d3.svg.arc()
+        .innerRadius(radius - 100)
+        .outerRadius(radius - 50);
+
+    var svg = d3.select("body").append("svg")
+        .attr("width", width)
+        .attr("height", height)
+        .append("g")
+        .attr("transform", "translate(" + width / 2 + "," + height / 2 + ")");
+
+    var path = svg.selectAll("path")
+        .data(pie(dataset.apples))
+        .enter().append("path")
+        .attr("fill", function(d, i) { return color(i); })
+        .attr("d", arc);
+
+    // get a reference to our SVG object and add the SVG NS
+    var svgGraph = d3.select('svg')
+        .attr('xmlns', 'http://www.w3.org/2000/svg');
+    var svgXML = (new xmldom.XMLSerializer()).serializeToString(svgGraph[0][0]);
+    // fs.writeFile('graph.svg', svgXML);
+
+    res.send(svgXML);
+}
