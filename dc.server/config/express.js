96537ebefc939d7a913dcc25804c6d38ccec99d7
Petar Petrov <petar.petrov.georgiev@gmail.com>
2014-04-21 14:13:30 +0200

    Added compress directive

--- dc.server/config/express.js
+++ dc.server/config/express.js
@@ -14,6 +14,7 @@ module.exports = function(app, config) {
     app.engine('ejs', require('ejs-locals'));
     app.set('view engine', 'ejs');
     app.use(express.favicon());
+    app.use(express.compress());
     app.use(express.logger('dev'));
     app.use(express.bodyParser());
     app.use(express.methodOverride());
